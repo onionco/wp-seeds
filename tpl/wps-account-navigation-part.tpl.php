7a58cd04a98d8b30554a9f87dce979d64b55db7a
Mikael Lindqvist <li.mikael@gmail.com>
2020-01-07 22:57:31 +0800

    checkcbf

--- tpl/wps-account-navigation-part.tpl.php
+++ tpl/wps-account-navigation-part.tpl.php
@@ -4,18 +4,18 @@ $seeds_account = intval( get_query_var( 'wpsaccount' ) );
 $send_seeds = intval( get_query_var( 'wpssend' ) );
 $request_seeds = intval( get_query_var( 'wpsrequest' ) );
 
-switch (1) {
-    case $seeds_account:
-        $active = 1;
-        break;
-    case $send_seeds:
-        $active = 2;
-        break;
-    case $request_seeds:
-        $active = 3;
-        break;
-    default:
-        $active = 0;
+switch ( 1 ) {
+	case $seeds_account:
+		$active = 1;
+		break;
+	case $send_seeds:
+		$active = 2;
+		break;
+	case $request_seeds:
+		$active = 3;
+		break;
+	default:
+		$active = 0;
 }
 
 $account = 'seeds-account';
@@ -24,15 +24,30 @@ $request = 'request';
 ?>
 
 <nav class="seeds-account-navigation">
-    <ul>
-        <li class="seeds-account-link <?php if ($active == 1) { echo 'active';} ?>">
-            <a href="<?php echo get_site_url() . '/' . $account  . '/' ?>" title="Seeds Account">Seeds Account</a>
-        </li>
-        <li class="seeds-account-link <?php if ($active == 2) { echo 'active';} ?>">
-            <a href="<?php echo get_site_url() . '/' . $account . '/' . $send  . '/' ?>" title="Send Seeds">Send Seeds</a>
-        </li>
-        <li class="seeds-account-link <?php if ($active == 3) { echo 'active';} ?>">
-            <a href="<?php echo get_site_url() . '/' . $account . '/' . $request  . '/' ?>" title="Send Seeds">Request Seeds</a>
-        </li>
-    </ul>
+	<ul>
+		<li class="seeds-account-link 
+		<?php
+		if ( $active == 1 ) {
+			echo 'active';}
+		?>
+		">
+			<a href="<?php echo get_site_url() . '/' . $account . '/'; ?>" title="Seeds Account">Seeds Account</a>
+		</li>
+		<li class="seeds-account-link 
+		<?php
+		if ( $active == 2 ) {
+			echo 'active';}
+		?>
+		">
+			<a href="<?php echo get_site_url() . '/' . $account . '/' . $send . '/'; ?>" title="Send Seeds">Send Seeds</a>
+		</li>
+		<li class="seeds-account-link 
+		<?php
+		if ( $active == 3 ) {
+			echo 'active';}
+		?>
+		">
+			<a href="<?php echo get_site_url() . '/' . $account . '/' . $request . '/'; ?>" title="Send Seeds">Request Seeds</a>
+		</li>
+	</ul>
 </nav>
