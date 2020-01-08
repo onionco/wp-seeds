5542a9e80a2765722e4529deb6870005fd548a65
Mikael Lindqvist <li.mikael@gmail.com>
2020-01-08 14:46:33 +0800

    fixed code sniffer checks

--- wp-seeds.php
+++ wp-seeds.php
@@ -269,7 +269,7 @@ function seeds_account_shortcode( $atts = array() ) {
 				
 				<?php display_template( dirname( __FILE__ ) . '/tpl/wps-account-balance-part.tpl.php' ); ?>
 				
-				<?php echo wps_history_sc( array() ); ?>
+				<?php wps_history_sc( array() ); ?>
 			</div>
 			
 		</div>
